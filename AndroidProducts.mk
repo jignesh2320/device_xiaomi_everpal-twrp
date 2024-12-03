f31315bd7272eac505015c59eae0573c0cc3f68a
Buildbot_Jignesh <jigneshshukla212@gmail.com>
2024-12-03 23:41:14 +0530

    everpal :change build  evergo to everpal

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,9 +16,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_evergo.mk
+    $(LOCAL_DIR)/twrp_everpal.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_evergo-user \
-    twrp_evergo-userdebug \
-    twrp_evergo-eng
+    twrp_everpal-user \
+    twrp_everpal-userdebug \
+    twrp_everpal-eng
